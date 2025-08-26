4b1c57f020a5666d547b7f93fbe52fb1df62651a
thai.nbug <null>
2025-08-26 21:04:43 +0700

    oopsie B|

--- src/query-provider.tsx
+++ src/query-provider.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC } from "react";
+import { RecoilRoot } from "recoil";
 
 interface PuckProviderProps {
   children?: React.ReactNode;
@@ -10,8 +11,12 @@ const queryClient = new QueryClient();
 
 export const PuckProvider: FC<PuckProviderProps> = ({ children }) => {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider value={defaultSystem}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </ChakraProvider>
+    </RecoilRoot>
   );
 };
